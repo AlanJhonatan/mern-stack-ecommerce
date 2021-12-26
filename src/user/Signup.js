49e4aebb3b8b7ff0159ee0653f390aa0269620e2
AlanJhonatan <alan.amorim.franca@gmail.com>
2021-12-26 00:12:08 -0300

    Added: input change handler to keep state object

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,15 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Layout from '../core/Layout';
-import { API } from "../config";
+// import { API } from '../config';
 
-const Signup = () => (
-  <Layout
-    title='Signup'
-    description='Signup to Node React E-commerce App'
-  >
-    {API}
-  </Layout>
-);
+const Signup = () => {
+  const [ values, setValues ] = useState({
+    name: '',
+    email: '',
+    password: '',
+    error: '',
+    success: false,
+  });
+
+  const handleChange = (event, name) => {
+    setValues({...values, error: false, [name]: event.target.value});
+  }
+
+  const signUpForm = () => (
+    <form>
+      <div className='form-group'>
+        <label className='text-muted'>Name</label>
+        <input
+          type='text'
+          onChange={(event) => handleChange(event, 'name')}
+          className='form-control'
+        />
+      </div>
+
+      <div className='form-group'>
+        <label className='text-muted'>Email</label>
+        <input
+          type='email'
+          onChange={(event) => handleChange(event, 'email')}
+          className='form-control'
+        />
+      </div>
+
+      <div className='form-group'>
+        <label className='text-muted'>Password</label>
+        <input
+          type='password'
+          onChange={(event) => handleChange(event, 'password')}
+          className='form-control'
+        />
+      </div>
+
+      <button className='btn btn-primary'>
+        Submit
+      </button>
+    </form>
+  );
+
+  return (
+    <Layout
+      title='Signup'
+      description='Signup to Node React E-commerce App'
+      className='container col-md-8 offset-md-2'
+    >
+      {signUpForm()}
+      {JSON.stringify(values)}
+    </Layout>
+  );
+}
 
 export default Signup;
