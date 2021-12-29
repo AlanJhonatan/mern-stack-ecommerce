5032eebf90271fa3a5994d334af37e6ba0d63c1e
AlanJhonatan <alan.amorim.franca@gmail.com>
2021-12-28 21:46:03 -0300

    Implemented: authenticate method usage

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import Layout from '../core/Layout';
 
-import { signin } from '../auth';
+import { signin, authenticate } from '../auth';
 
 const Signin = () => {
   const [values, setValues] = useState({
@@ -27,8 +27,9 @@ const Signin = () => {
         setValues({ ...values, error: data.error, loading: false });
         return;
       }
-
-      setValues({ ...values, redirectToReferrer: true });
+      authenticate(data, () => {
+        setValues({ ...values, redirectToReferrer: true });
+      });
     });
   };
 
@@ -82,8 +83,8 @@ const Signin = () => {
 
   return (
     <Layout
-      title='Signup'
-      description='Signup to Node React E-commerce App'
+      title='Signin'
+      description='Signin to Node React E-commerce App'
       className='container col-md-8 offset-md-2'
     >
       {showError()}
