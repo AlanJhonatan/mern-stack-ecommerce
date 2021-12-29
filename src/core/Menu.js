e93c2dbe0d32201bb2d8bc5d2523d176afaa5bd2
AlanJhonatan <alan.amorim.franca@gmail.com>
2021-12-28 21:52:41 -0300

    Implemented: signout menu on tab

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,45 +1,57 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import { signout } from '../auth';
 
 const isActive = (history, path) => {
-  if(history.location.pathname === path) {
-    return { color: '#ff9900'};
+  if (history.location.pathname === path) {
+    return { color: '#ff9900' };
   }
 
-  return { color: '#ffffff'};
-}
+  return { color: '#ffffff' };
+};
 
 const Menu = ({ history }) => (
   <div>
     <ul className='nav nav-tabs bg-primary'>
-
       <li className='nav-item'>
-        <Link
-        className='nav-link'
-        style={isActive(history, '/')}
-        to="/">
+        <Link className='nav-link' style={isActive(history, '/')} to='/'>
           Home
         </Link>
       </li>
 
       <li className='nav-item'>
         <Link
-        className='nav-link'
-        style={isActive(history, '/signin')}
-        to="/signin">
+          className='nav-link'
+          style={isActive(history, '/signin')}
+          to='/signin'
+        >
           Signin
         </Link>
       </li>
 
       <li className='nav-item'>
         <Link
-        className='nav-link'
-        style={isActive(history, '/singup')}
-        to="/signup">
+          className='nav-link'
+          style={isActive(history, '/singup')}
+          to='/signup'
+        >
           Signup
         </Link>
       </li>
 
+      <li className='nav-item'>
+        <span
+          className='nav-link'
+          style={{ cursor: 'pointer', color: '#fff' }}
+          onClick={() =>
+            signout(() => {
+              history.push('/');
+            })
+          }
+        >
+          Signout
+        </span>
+      </li>
     </ul>
   </div>
 );
