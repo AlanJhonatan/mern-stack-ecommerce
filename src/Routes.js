d6d716b016761e9e38cf1469c51f812f41fbac49
AlanJhonatan <alan.amorim.franca@gmail.com>
2021-12-29 23:36:30 -0300

    Implemented: Private Route on Routes component

--- src/Routes.js
+++ src/Routes.js
@@ -4,17 +4,21 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Signup from './user/Signup';
 import Signin from './user/Signin';
 import Home from './core/Home';
+import Dashboard from './user/UserDashboard';
+
+import PrivateRoute from './auth/PrivateRoute';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/signin" exact component={Signin} />
-        <Route path="/signup" exact component={Signup} />
+        <Route path='/' exact component={Home} />
+        <Route path='/signin' exact component={Signin} />
+        <Route path='/signup' exact component={Signup} />
+        <PrivateRoute path='/dashboard' exact component={Dashboard} />
       </Switch>
     </BrowserRouter>
   );
-}
+};
 
 export default Routes;
