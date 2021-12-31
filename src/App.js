87ec2760dafc9bd0e92195aa6a61f90b4925c35d
AlanJhonatan <alan.amorim.franca@gmail.com>
2021-12-30 21:03:21 -0300

    Removed: non used component

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import Signup from './user/Signup';
-import Signin from './user/Signin';
-
-const App = () => <h1>Hello ! <Signup /> <Signin /> </h1>
-
-export default App;
