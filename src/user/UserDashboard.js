43195f684ac9b544dbbfcd6b7e96710a725f704d
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-01-02 15:58:30 -0300

    Refactor: fixed user dashboard code

--- src/user/UserDashboard.js
+++ src/user/UserDashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../core/Layout';
+import { Link } from 'react-router-dom';
 
 import { isAuthenticated } from '../auth';
 
@@ -8,12 +9,28 @@ const Dashboard = () => {
     user: { _id, name, email, role },
   } = isAuthenticated();
 
-  return (
-    <Layout
-      title='Dashboard Title'
-      description='User Dashboard Description'
-      className='container'
-    >
+  const userLinks = () => {
+    return (
+      <div className='card'>
+        <h4 className='card-header'>User Links</h4>
+        <ul className='list-group'>
+          <li className='list-group-item'>
+            <Link className='nav-link' to='/cart'>
+              My Cart
+            </Link>
+          </li>
+          <li className='list-group-item'>
+            <Link className='nav-link' to='/profile/update'>
+              Update Profile
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  const userInformation = () => {
+    return (
       <div className='card mb-5'>
         <h3 className='card-header'>User information</h3>
         <ul className='list-group'>
@@ -24,12 +41,31 @@ const Dashboard = () => {
           </li>
         </ul>
       </div>
+    );
+  };
 
+  const purchaseHistory = () => {
+    return (
       <div className='card mb-5'>
         <h3 className='card-header'>Purchase history</h3>
         <ul className='list-group'>
           <li className='list-group-item'>history</li>
         </ul>
+      </div>
+    );
+  };
+
+  return (
+    <Layout
+      title='Dashboard Title'
+      description={`Hello ${name}`}
+      className='container'
+    >
+      <div className='row'>
+        <div className='col-3'>{userLinks()}</div>
+        <div className='col-9'>
+          {userInformation()} {purchaseHistory()}
+        </div>
       </div>
     </Layout>
   );
