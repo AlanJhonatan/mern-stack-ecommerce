560026f27f85c31acbe0ca0e773940ae946b6779
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-01-03 20:54:20 -0300

    Fix: redirect to home page if already authenticated

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -58,6 +58,9 @@ const Signin = () => {
         return <Redirect to='/user/dashboard' />;
       }
     }
+    if (isAuthenticated()) {
+      return <Redirect to='/' />;
+    }
   };
 
   const signinForm = () => (
