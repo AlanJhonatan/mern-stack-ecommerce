282d0e048a4e1912b3f7893515eccd5f4b5339ca
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-01-03 20:53:31 -0300

    Added: Admin Routes for filter access on Dashboard

--- src/Routes.js
+++ src/Routes.js
@@ -4,9 +4,12 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Signup from './user/Signup';
 import Signin from './user/Signin';
 import Home from './core/Home';
+
 import Dashboard from './user/UserDashboard';
+import AdminDashboard from './user/AdminDashboard';
 
 import PrivateRoute from './auth/PrivateRoute';
+import AdminRoute from './auth/AdminRoute';
 
 const Routes = () => {
   return (
@@ -16,6 +19,7 @@ const Routes = () => {
         <Route path='/signin' exact component={Signin} />
         <Route path='/signup' exact component={Signup} />
         <PrivateRoute path='/user/dashboard' exact component={Dashboard} />
+        <AdminRoute path='/admin/dashboard' exact component={AdminDashboard} />
       </Switch>
     </BrowserRouter>
   );
