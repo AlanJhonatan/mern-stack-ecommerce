0e22baf47228bea0250ef55e0d5811189e45b663
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-01-04 21:00:34 -0300

    Added: new admin route to add products

--- src/Routes.js
+++ src/Routes.js
@@ -12,6 +12,7 @@ import PrivateRoute from './auth/PrivateRoute';
 import AdminRoute from './auth/AdminRoute';
 
 import AddCategory from './admin/AddCategory';
+import AddProduct from './admin/AddProduct';
 
 const Routes = () => {
   return (
@@ -23,6 +24,7 @@ const Routes = () => {
         <PrivateRoute path='/user/dashboard' exact component={Dashboard} />
         <AdminRoute path='/admin/dashboard' exact component={AdminDashboard} />
         <AdminRoute path='/create/category' exact component={AddCategory} />
+        <AdminRoute path='/create/product' exact component={AddProduct} />
       </Switch>
     </BrowserRouter>
   );
