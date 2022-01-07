e0377440675b28d5bf1abf915a00b763cf87a354
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-01-06 22:14:22 -0300

    Fixed: import orders

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
+import Card from './Card';
 import Layout from './Layout';
 
 import { getProducts } from './apiCore';
-import Card from './Card';
 
 const Home = () => {
   const [productsBySell, setProductsBySell] = useState([]);
