1d13482032e2b7762921b4db3906006d748932bc
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-01-07 22:38:56 -0300

    Added: custom style using jumbotron

--- src/core/Layout.js
+++ src/core/Layout.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React from 'react';
 
 import Menu from './Menu';
+import '../styles.css';
 
 const Layout = ({
   title = 'Title',
   description = 'Description',
   className,
-  children
+  children,
 }) => (
   <div>
     <Menu />
-    <div className="jumbotron">
+    <div className='jumbotron'>
       <h2>{title}</h2>
-      <p className="lead">{description}</p>
+      <p className='lead'>{description}</p>
     </div>
     <div className={className}>{children}</div>
   </div>
