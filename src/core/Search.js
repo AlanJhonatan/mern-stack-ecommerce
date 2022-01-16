323f62b0774f74d45afc9d72fc6d7415418947b8
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-01-15 23:20:29 -0300

    Fixed: handling search submit with text and category Id

--- src/core/Search.js
+++ src/core/Search.js
@@ -27,10 +27,52 @@ const Search = () => {
     loadCategories();
   }, []);
 
+  const handleChange = (name) => (event) => {
+    setData({ ...data, [name]: event.target.value, searched: false });
+  };
+
+  const searchData = () => {
+    console.log(search, category);
+  };
+
+  const searchSubmit = (event) => {
+    event.preventDefault();
+    searchData();
+  };
+
+  const searchForm = () => (
+    <form onSubmit={searchSubmit}>
+      <span className='input-group-text'>
+        <div className='input-group input-group-lg'>
+          <div className='input-group-prepend'>
+            <select className='btn mr-2' onChange={handleChange('category')}>
+              <option value='All'>Pick Category</option>
+              {categories.map((c, idx) => (
+                <option key={idx} value={c._id}>
+                  {c.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <input
+            type='search'
+            className='form-control'
+            onChange={handleChange('search')}
+            placeholder='Search by name'
+          />
+        </div>
+        <div className='btn input-group-append' style={{ border: 'none' }}>
+          <button className='input-group-text' onClick={searchSubmit}>
+            Search
+          </button>
+        </div>
+      </span>
+    </form>
+  );
+
   return (
-    <div>
-      <h2>Search Bar</h2>
-      {JSON.stringify(categories)}
+    <div className='row'>
+      <div className='container mb-3'>{searchForm()}</div>
     </div>
   );
 };
