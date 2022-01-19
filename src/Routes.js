cd438b60b3994a35f02a3be3e21c6339c7737453
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-01-19 11:26:58 -0300

    Added: new Product component to Routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Product from './core/Product';
 import Signup from './user/Signup';
 import Signin from './user/Signin';
 import Home from './core/Home';
@@ -20,6 +21,7 @@ const Routes = () => {
     <BrowserRouter>
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/product/:productId' exact component={Product} />
         <Route path='/signin' exact component={Signin} />
         <Route path='/signup' exact component={Signup} />
         <Route path='/shop' exact component={Shop} />
