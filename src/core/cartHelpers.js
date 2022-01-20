e185e246ce28a14c09cfd21f3fc0d116eeea84fd
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-01-20 08:57:11 -0300

    Created: cart helper to handle all localStorage operations

---
+++ src/core/cartHelpers.js
@@ -0,0 +1,27 @@
+export const addItem = (item, next) => {
+  let cart = [];
+
+  if (typeof window === 'undefined') {
+    console.log('Error: Window object is undefined');
+    return;
+  }
+
+  if (localStorage.getItem('cart')) {
+    cart = JSON.parse(localStorage.getItem('cart'));
+  }
+
+  cart.push({
+    ...item,
+    count: 1,
+  });
+
+  // remove duplicates
+  // build an array from new Set and turn it back into array using Array.from
+  // later we can re-map it
+  cart = Array.from(new Set(cart.map((product) => product._id))).map((id) =>
+    cart.find((p) => p._id === id)
+  );
+
+  localStorage.setItem('cart', JSON.stringify(cart));
+  next();
+};
