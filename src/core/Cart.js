4c343a31108bd8df6d9273215e80dde3c499c474
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-01-24 09:34:01 -0300

    Added: card update prop

--- src/core/Cart.js
+++ src/core/Cart.js
@@ -19,7 +19,12 @@ const Cart = () => {
         <h2>Your cart has {[...items].length} items</h2>
         <hr />
         {items.map((product, idx) => (
-          <Card key={idx} product={product} showAddToCartButton={false}></Card>
+          <Card
+            key={idx}
+            product={product}
+            showAddToCartButton={false}
+            cartUpdate={true}
+          />
         ))}
       </div>
     );
