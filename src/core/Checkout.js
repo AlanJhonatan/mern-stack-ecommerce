d7b2ecb3b46a2aefadaee2064f3b0382fe3a9ee3
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-01-26 16:50:44 -0300

    Created: basic Checkout component to handle all products

---
+++ src/core/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import Card from './Card';
+// import Search from './Search';
+
+// import { getProducts } from './apiCore';
+
+const Checkout = ({ products }) => {
+  return <p>products: {JSON.stringify(products)}</p>;
+};
+
+export default Checkout;
