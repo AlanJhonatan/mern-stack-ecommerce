385a4f7fd9d1e357b1446e1454a2a4fa52607684
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-02-04 08:04:54 -0300

    chore: implemented payment process at backend

--- backend/controllers/braintree.js
+++ backend/controllers/braintree.js
@@ -19,3 +19,26 @@ exports.generateToken = (req, res) => {
     res.send(response);
   });
 };
+
+exports.processPayment = (req, res) => {
+  let nonceFromTheClient = req.body.paymentMethodNonce;
+  let amountFromTheClient = req.body.amount;
+
+  //charge user
+  let newTransaction = gateway.transaction.sale(
+    {
+      amount: amountFromTheClient,
+      paymentMethodNonce: nonceFromTheClient,
+      options: {
+        submitForSettlement: true,
+      },
+    },
+    (error, result) => {
+      if (error) {
+        res.status(500).json(error);
+      }
+
+      res.json(result);
+    }
+  );
+};
--- backend/routes/braintree.js
+++ backend/routes/braintree.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const { requireSigin, isAuth } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
-const { generateToken } = require('../controllers/braintree');
+const { generateToken, processPayment } = require('../controllers/braintree');
 
 router.get('/braintree/getToken/:userId', requireSigin, isAuth, generateToken);
+router.post('/braintree/payment/:userId', requireSigin, isAuth, processPayment);
 
 router.param('userId', userById);
 
